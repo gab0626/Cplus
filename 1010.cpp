2182c16421b4518ab420f0eea22bc69248cfb691
gab0626 <kyhk.0626@outlook.jp>
2015-10-10 09:11:00 +0900

    ニューラリーX

---
+++ 1010.cpp
@@ -0,0 +1,57 @@
+﻿#include "lib/framework.hpp"
+#include "Class.h"
+#include "Singlton.h"
+#include "SceneManager.h"
+ 
+int main() {
+	env;
+
+	Font font("res/msgothic.ttc");
+	font.size(50);
+
+	enum {
+		TITLE,
+		STAGE1,
+		RESULT
+	};
+
+	int e = 0;
+
+  while (env.isOpen()) {
+	  
+    env.begin();
+
+	if (!env.isOpen())
+	{
+		return 0;
+	}
+
+	switch (e)
+	{
+	case TITLE:
+		font.draw("ニューラリーX", Vec2f(500, 500),Color::white);
+		break;
+
+	case STAGE1:
+		font.draw("ステージ１", Vec2f(500,500), Color::white);
+		break;
+	case RESULT:
+		font.draw("リザルト", Vec2f(504, 502), Color::white);
+		break;
+	}
+	
+
+	if (env.isPushButton(Mouse::LEFT))
+	{
+		e = (e + 1);
+		if (e >= 3)
+		{
+			e = 0;
+		}
+	}
+
+
+	
+    env.end();
+  }
+}
